90a2082ac28951708a63890b0a01de6c5b8bec76
Kulinich Vlad <kulinich_v@hotmail.com>
2017-11-10 00:57:12 +0200

    share config3

--- src/Test/AllTests.java
+++ src/Test/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestsExist.class, TestsRJ.class, TestsComplex.class, TestsSimple.class })
+@SuiteClasses({ TestsExist.class, TestsRJ.class, TestsComplex.class, TestsSimple.class, TestsApi.class })
 public class AllTests {
 
 }
